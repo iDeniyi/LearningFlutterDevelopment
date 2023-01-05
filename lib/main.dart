6dea2ba344045aca67a29fe982512cf7771be8c4
iDeniyi <71891768+iDeniyi@users.noreply.github.com>
2023-01-04 19:18:42 -0700

    Add files via upload
    Completed the BusinessCardApp with my contact information.

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/user.jpeg'),
+            ),
+            Text(
+              'Inioluwa Adeniyi',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.blueGrey[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 20.0, width: 150.0, child: Divider(color: Colors.blueGrey.shade100)),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text('+1 (780) 655 7162',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      )),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text('adeniyipraiseini@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      )),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
